221f273c172a7e6501c41e0aea93d7072e361599
f1rset <stekostant@gmail.com>
2024-10-28 10:17:04 +0200

    done

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,21 @@
 package flower.store;
 
+import lombok.Getter;
+import lombok.Setter;
+@Getter
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+
+    public FlowerPack(Flower flower1, int quantity1){
+        this.flower = flower1;
+        this.quantity = quantity1;
+    }
+
+    public Flower getFlower(){
+        return this.flower;
+    }
+    public double getPrice(){
+        return flower.getPrice()*quantity;
+    }
 }
